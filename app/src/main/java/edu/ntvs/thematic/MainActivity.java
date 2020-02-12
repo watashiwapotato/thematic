606c56977075e52b01c8e806d94d8d2967f22ce5
potato <matt104072@gmail.com>
2020-02-12 17:41:23 +0800

    1.0.0 webview 縮放問題

--- app/src/main/java/edu/ntvs/thematic/MainActivity.java
+++ app/src/main/java/edu/ntvs/thematic/MainActivity.java
@@ -3,6 +3,8 @@ package edu.ntvs.thematic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webView.loadUrl("http://potato-commander.ddns.net/NewTaipei/");
     }
 }
